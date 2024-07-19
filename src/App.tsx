0b85ff75b8d67384e06f4804018ea76d384b9662
humbl3LilaS <sabishinekobebe@gmail.com>
2024-07-19 20:16:55 +0630

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {Button} from "@/components/ui/button";
+
+
+function App() {
+
+
+    return (
+        <>
+            {/*<h1 className={"text-stone-700"}>Hello stupid ass fker</h1>*/}
+            <Button>Click me</Button>
+        </>
+    );
+}
+
+export default App;
