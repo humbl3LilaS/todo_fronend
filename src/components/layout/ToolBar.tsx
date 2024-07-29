f7f0756b1b1c118f670601bb37c0c8ba822b3bf6
humbl3LilaS <sabishinekobebe@gmail.com>
2024-07-29 23:36:01 +0630

    fix: toolbar not uploaded in previous commit

---
+++ src/components/layout/ToolBar.tsx
@@ -0,0 +1,13 @@
+import ProfileAvatar from "@/components/util/ProfileAvatar.tsx";
+import ProfileDropdown from "@/components/util/ProfileDropdown.tsx";
+
+export default function ToolBar() {
+    return (
+        <div className={"py-6"}>
+            <ul className={"flex justify-end items-center"}>
+                {/*TODO:Add theme switcher here*/}
+                <ProfileDropdown/>
+            </ul>
+        </div>
+    );
+}
