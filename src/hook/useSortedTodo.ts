81935f87c22bb2b0ad1ca0a15f7ab44d155061e2
humbl3LilaS <sabishinekobebe@gmail.com>
2024-08-23 12:18:38 +0630

    add: sorting functionality , alphabetically and creation date

--- src/hook/useSortedTodo.ts
+++ src/hook/useSortedTodo.ts
@@ -1,8 +1,10 @@
 import {TTodo} from "@/types/apiResponseType.ts";
 import {useSortOption} from "@/store/sortOptionStore.ts";
+import {differenceInDays} from "date-fns";
 
 export const useSortedTodo = (data: TTodo[] | undefined) => {
     const {sortBy} = useSortOption();
+    // TODO: add sorting functionality to be sort via asc or desc
     switch (sortBy) {
         case 'dueDate':
             return data?.slice().sort((a, b) => {
@@ -11,5 +13,18 @@ export const useSortedTodo = (data: TTodo[] | undefined) => {
                 if (!b.dueAt) return -1;
                 return b.dueAt - a.dueAt;
             });
+        case "alphabetically":
+            return data?.slice().sort((a, b) => {
+                return a.content.localeCompare(b.content)
+            });
+        // TODO: Implement sorting by Importance
+        // TODO: Implement sorting by Priority
+        case "creationDate":
+            return data?.slice().sort((a, b) => {
+                if (differenceInDays(a.createdAt, b.createdAt) === 0) return 0;
+                return differenceInDays(a.createdAt, b.createdAt) > 0 ? 1 : -1;
+            })
+        default:
+            return data;
     }
 }
