b2ed9c14c5bc66ef474c6db7bbe2fcaa4f0c7c03
humbl3LilaS <sabishinekobebe@gmail.com>
2024-08-23 12:10:48 +0630

    add: implement the functionality of SortSelectBox

--- src/components/util/SortSelectBox.tsx
+++ src/components/util/SortSelectBox.tsx
@@ -1,19 +1,19 @@
 import {Select, SelectContent, SelectItem, SelectTrigger} from "@/components/ui/select.tsx";
 import SortBtn from "@/components/button/SortBtn.tsx";
+import {useSortOption} from "@/store/sortOptionStore.ts";
 
 export default function SortSelectBox() {
+    const {setSortBy} = useSortOption();
     return (
-        <Select>
+        <Select onValueChange={setSortBy} defaultValue={"dueDate"}>
             <SelectTrigger className={"w-fit py-2 px-4"}>
                 <SortBtn/>
             </SelectTrigger>
             <SelectContent className={"border-0 outline-0"}>
-                <SelectItem value={"importance"}>
-                    Importance
-                </SelectItem>
-                <SelectItem value={"due"}>Due date</SelectItem>
-                <SelectItem value={"alphabetically"}>Alphabetically</SelectItem>
-                <SelectItem value={"createdDate"}>Creation Date</SelectItem>
+                <SelectItem value={"dueDate"} key={"dueDate"}>Due date</SelectItem>
+                <SelectItem value={"importance"} key={"importance"}>Importance</SelectItem>
+                <SelectItem value={"alphabetically"} key={"alphabetically"}>Alphabetically</SelectItem>
+                <SelectItem value={"creationDate"} key={"creationDate"}>Creation Date</SelectItem>
             </SelectContent>
         </Select>
     );
