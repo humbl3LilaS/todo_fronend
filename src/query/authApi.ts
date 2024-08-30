a568fd6b70bb82fa9f99a7a9ae8bd9c361ce9c44
humbl3LilaS <sabishinekobebe@gmail.com>
2024-08-30 22:15:52 +0630

    fix: authApi

--- src/query/authApi.ts
+++ src/query/authApi.ts
@@ -1,10 +1,14 @@
-import {axiosInstance} from "@/query/axiosInstance.ts";
 import {TAuth, TAuthRoute, TLogin, TSignUp} from "@/types/authType.ts";
-import {AxiosResponse} from "axios";
+import axios, {AxiosResponse} from "axios";
 
 
 export const auth = async <T extends TAuthRoute>(path: T, payload: T extends "/auth/login" ? TLogin : TSignUp) => {
-    const jwt_key = await axiosInstance.post<TAuth, AxiosResponse<TAuth>>(path, payload)
-        .then(res => res.data);
+    const jwt_key = await axios.request<TAuth, AxiosResponse<TAuth>>(
+        {
+            baseURL: `${import.meta.env.VITE_API_ENDPOINT}${path}`,
+            method: "POST",
+            data: payload,
+        }
+    ).then(res => res.data);
     return jwt_key;
 };
